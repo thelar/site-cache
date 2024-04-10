97c100ca116b00023e2087716b4ed69d0bda349f
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-04-10 14:29:06 +0100

    test socket

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get(root, (req, res) => {
     res.send('Hello World!');
 });
 
-server.listen(3000, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
