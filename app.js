4e6a514ea45e41409b6c4819600865ea4eadfa92
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-04-10 14:13:41 +0100

    test socket

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const root = '/site-cache';
+const axios = require('axios');
+const path_to_ajax = 'https://4x4tyres.localhost/';
 
 app.get(root, (req, res) => {
     res.send('Hello World!');
+});
+
+app.get(root + '/start', (req, res) => {
+    const sendGetRequest = async() => {
+        try {
+            const resp = await axios.get(path_to_ajax + 'fbf_cache?action=get_manufacturers');
+            console.log(resp.data);
+            res.json(resp.data);
+        }catch(err){
+            console.log(err);
+            res.send('ERROR: ' + err.code);
+        }
+    }
+    sendGetRequest();
 });
 
 app.get(root + '/console', function(req, res) {
