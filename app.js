7a59dde38f24b0745d7f4ea5c8a4818b7dc746bf
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-04-10 11:03:46 +0100

    test socket

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-require('dotenv').config();
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 const root = '/site-cache';
-const axios = require('axios');
-const path_to_ajax = 'https://4x4tyres.localhost/'
 
-app.get(root + '/console', function(req, res) {
-    res.sendFile('index.html', { root: __dirname });
+app.get(root, (req, res) => {
+    res.send('Hello World!');
 });
 
 app.get(root + '/start', (req, res) => {
-    const sendGetRequest = async() => {
-        try {
-            const resp = await axios.get(path_to_ajax + 'fbf_cache?action=get_manufacturers');
-            console.log(resp.data);
-            res.json(resp.data);
-        }catch(err){
-            console.log(err);
-            res.send('ERROR: ' + err.code);
-        }
-    }
-    sendGetRequest();
-});
-
-io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
+    res.json({
+        "message": "started"
     });
 });
 
-app.listen(3000, function() {
-    console.log(`Listening on port ${port}`);
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
